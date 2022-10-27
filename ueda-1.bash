9656747267ab7829fde0221e260dccaacf1ba019
Masaki Mitani <pasapasamasaki0@gmail.com>
2022-10-27 12:00:26 +0900

    Add a command

---
+++ ueda-1.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ng () {
+	echo $[1]行目が違うよ #$[1]はngの一番目の引数
+	ret=1
+}
+
+ret=0
+a=ueda
+[ "$a" = ueda] || ng "$LINENO" #LINENOは行の番号の入る変数,LINENOが$[]の中に入る
+
+exit $ret
---
+++ ueda.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+a=ueda
+[ "$a" = ueda ]
+echo $?
